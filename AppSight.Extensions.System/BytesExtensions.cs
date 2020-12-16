77dc8a3a3d7dbc2083595cb88436f570832f7509
yossiejs <yossiejs@gmail.com>
2020-12-17 02:11:52 +0900

    Add AppSight.Security.Gryptography, Add AppSight.Extensions.System (#1)
    * add AppSight.Security, add AppSight.System.Security.Cryptography
    
    * rename
    
    * rename

---
+++ AppSight.Extensions.System/BytesExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppSight.Extensions.System
+{
+    public static class BytesExtensions
+    {
+		public static string ToHashString(this byte[] bytes)
+		{
+			return BitConverter.ToString(bytes).Replace("-", "").ToLower();
+		}
+	}
+}
---
+++ AppSight.Security.Cryptography/HashType.cs
@@ -0,0 +1,10 @@
+﻿namespace AppSight.Security.Cryptography
+{
+    public enum HashType
+    {
+        MD5,
+        SHA1,
+        SHA256,
+        SHA512,
+    }
+}
